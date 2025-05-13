0c7f17c629a09907961c3b5ae66cc313c349495d
agentpppp <bondsarthakpbt@gmail.com>
2025-05-14 03:10:27 +0530

    intial commit

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  optimizeDeps: {
+    exclude: ['@electric-sql/pglite'],
+  },
+  plugins: [tailwindcss(),react()],
+  worker: {
+    format: 'es',
+  },
+})
